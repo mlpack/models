3e758405bbb0e64336126a8043b28050d00afb42
Aakash Kaushik <kaushikaakash7539@gmail.com>
2021-06-19 14:32:55 +0530

    added tests

--- tests/ffn_model_tests.cpp
+++ tests/ffn_model_tests.cpp
@@ -55,18 +55,45 @@ TEST_CASE("YOLOV1ModelTest", "[FFNModelsTests]")
   REQUIRE(output.n_rows == (7 * 7 * (5 * 2 + 20)));
 }
 
+/**
+ * Simple test for ResNet models.
+ */
 TEST_CASE("ResNetModelTest", "[FFNModelsTests]")
 {
   ResNet18 resnet18(3, 224, 224);
   arma::mat input(224 * 224 * 3, 1), output;
   input.ones();
 
+  // Check output shape for resnet18.
   resnet18.GetModel().Predict(input, output);
   REQUIRE(output.n_cols == 1);
   REQUIRE(output.n_rows == 1000);
 
   ResNet34 resnet34(3, 224, 224);
+
+  // Check output shape for resnet34.
   resnet34.GetModel().Predict(input, output);
   REQUIRE(output.n_cols == 1);
   REQUIRE(output.n_rows == 1000);
+
+  ResNet50 resnet50(3, 224, 224);
+
+  // Check output shape for resnet50.
+  resnet50.GetModel().Predict(input, output);
+  REQUIRE(output.n_cols == 1);
+  REQUIRE(output.n_rows == 1000);
+
+  ResNet101 resnet101(3, 224, 224);
+  
+  // Check output shape for resnet101.
+  resnet101.GetModel().Predict(input, output);
+  REQUIRE(output.n_cols == 1);
+  REQUIRE(output.n_rows == 1000);
+
+  ResNet152 resnet152(3, 224, 224);
+  
+  // Check output shape for resnet152.
+  resnet152.GetModel().Predict(input, output);
+  REQUIRE(output.n_cols == 1);
+  REQUIRE(output.n_rows == 1000);s
 }
