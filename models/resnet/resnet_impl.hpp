ba5e008765a1890ca400f36b842239c6aa8627eb
Aakash Kaushik <kaushikaakash7539@gmail.com>
2021-06-26 21:58:08 +0530

    add 0 to return

--- models/resnet/resnet_impl.hpp
+++ models/resnet/resnet_impl.hpp
@@ -65,7 +65,7 @@ ResNet<OutputLayerType, InitializationRuleType, ResNetVersion>::ResNet(
   {
     LoadModel("./../weights/resnet/resnet" + std::to_string(ResNetVersion) +
         "_imagenet.bin");
-    return;
+    return 0;
   }
 
   // Config for different Versions.
